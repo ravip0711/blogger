11cc2dd13cdcffa2c69691d6f70d3453a14ff1ed
Ravi P <ravip0711@gmail.com>
2016-01-12 20:26:44 -0700

    finished blog comments feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
- root to: "articles#index" 
- resources :articles
+  
+  root to: "articles#index" 
+  
+  resources :articles do
+    resources :comments
+  end
 end
