8bfb5cdb8b6dc1a84bdc2002851869ddd362a4d8
Ravi P <ravip0711@gmail.com>
2016-01-15 18:39:50 -0700

    tag feature completed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,19 @@
 class Article < ActiveRecord::Base
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+
+  def tag_list
+    self.tags.collect do |tag|
+    tag.name
+    end.join(", ")
+  end
+
+  def tag_list=(tags_string)
+    tag_names = tags_string.split(",").map{ |s| s.strip.downcase }.uniq
+    new_or_found_tags = tag_names.map{ |name| Tag.where(name: name).first_or_create }
+    self.tags = new_or_found_tags
+  end
+
 end
